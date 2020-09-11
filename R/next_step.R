8c1e1e0b7bb64ffa83b3c0da46b6a49f1023d524
Maxwell <allswellthatsmaxwell9@gmail.com>
2020-09-11 14:30:11 -0500

    plotted pnexts for multiple gammas

--- R/next_step.R
+++ R/next_step.R
@@ -3,9 +3,9 @@ library(ggplot2)
 library(magrittr)
 library(dplyr)
 
-p_step <- function(fn, params, from, stepsize=1) {
-  start <- do.call(partial(fn, from), params)
-  end <- do.call(partial(fn, from + stepsize), params)
+p_step <- function(shape, scale, from, stepsize=1) {
+  start <- pgamma(q=from, shape=shape, scale=scale)
+  end <- pgamma(q = from + stepsize, shape=shape, scale=scale)
   end - start
 }
 
@@ -16,54 +16,43 @@ make_distribution <- function(parameter_set, xs) {
                    name=glue::glue("shape={shape}, scale={scale}"))
 }
 
-distributions <- list(
-  "gamma" = list(p = pgamma, d = dgamma, x=seq(from=0, to=30, by=1),
-                 params=list(shape=7.5, scale=1)),
-  "normal" = list(p = pnorm, d = dnorm, x=seq(from=-4, to=6, by=0.01),
-                  params=list(mean=0, sd=1)))
-choice <- "gamma"
-distribution <- distributions[[choice]]
-
 parameter_sets <- list(
   list(shape=1, scale=2),
   list(shape=2, scale=2),
   list(shape=3, scale=2),
   list(shape=5, scale=1),
   list(shape=9, scale=0.5),
-  # list(shape=0.5, scale=1),
+  list(shape=0.5, scale=1),
   list(shape=7.5, scale=1))
-range <- seq(from=0, to=20, by=1)
+stepsize <- 1
+range <- seq(from=0, to=20, by=stepsize)
 
 distros_df <- lapply(parameter_sets, 
                      function(pset) make_distribution(pset, range)) %>%
   dplyr::bind_rows()
 
+distros_df %<>%
+  mutate(p_under = p_step(scale=scale, shape=shape, from=xs, stepsize=stepsize),
+         p_rest  = p_step(scale=scale, shape=shape, from=xs, stepsize=1000),
+         p_next_unnorm = p_under / p_rest) %>%
+  group_by(name) %>%
+  mutate(sum_p_next_unnorm = sum(p_next_unnorm)) %>%
+  mutate(p_next = p_next_unnorm / sum_p_next_unnorm) %>%
+  dplyr::ungroup()
+
 distros_df %>%
   group_by(name) %>%
-  summarize(sum(ys))
+  summarize(sum(ys), sum(p_next))
 
-distros_df %>%
-  ggplot(aes(x=xs, y=ys, color=name)) +
+wiki_plot <- distros_df %>%
+  ggplot(aes(x = xs, y = ys, color = name)) +
   geom_line() +
   theme_bw()
-################################################################################
 
-p_under <- function(from)
-  with(distribution, p_step(fn=p, params=params, from=from, stepsize=0.01))
-p_rest <- function(from) 
-  with(distribution, p_step(fn=p, params=params, from=from, stepsize=1000))
-ratio_next_to_rest <- function(from)
-  p_under(from) / p_rest(from)
-
-distribution$y <- distribution %$% do.call(partial(d, x), params)
-
-next_probas <- distribution %$% sapply(x, ratio_next_to_rest)
-
-dat <- with(distribution, tibble::tibble(x=x, y=y, p_next=next_probas))
-
-dat %>%
-  ggplot(aes(x = x)) +
-  geom_line(aes(y = y)) +
+plot_with_pnext <- distros_df %>%
+  ggplot(aes(x = xs)) +
+  geom_line(aes(y = ys), color = "black") +
   geom_line(aes(y = p_next), color = "purple") +
+  facet_wrap(~name, scales = "free_y") +
   theme_bw()
-
+plot_with_pnext
